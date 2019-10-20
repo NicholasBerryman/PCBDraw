275de8f320b504f491cafe1b3f5d1359db416028
unknown <Nicholas Berryman>
2019-10-20 15:42:37 +0800

    Removed Web-Start files

--- src/pcbdraw/PCBDraw.java
+++ src/pcbdraw/PCBDraw.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
